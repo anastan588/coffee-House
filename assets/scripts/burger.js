94fcde3d661f37efa5a1e717aa4e497341a37e8f
Anastasiya Andronova <anastasiya5.88@mail.ru>
2023-11-28 01:52:20 +0300

    feat: add burger-menu

--- assets/scripts/burger.js
+++ assets/scripts/burger.js
@@ -2,34 +2,47 @@ let burgerImage = document.querySelector(".burger");
 let menu = document.querySelector(".menu_block_burger");
 export let body = document.querySelector(".body");
 export let overlay = document.querySelector(".overlay");
+let burgerLine1 = document.querySelector(".line_1");
+let burgerLine2 = document.querySelector(".line_2");
+let menuItem = document.querySelectorAll(".nav_item");
+console.log(menu);
+console.log(body);
+console.log(overlay);
+console.log(burgerLine1);
+console.log(burgerLine2);
 
 function openOrCloseBurger(event) {
   event.stopPropagation();
-  //console.log(event);
+  console.log(event);
   //console.log(window.innerWidth);
   //console.log(typeof window.innerWidth);
   let styleOfOverlay = getComputedStyle(overlay);
   if (
     !menu.classList.contains("burger-open") &&
-    window.innerWidth <= 767 &&
+    window.innerWidth <= 768 &&
     styleOfOverlay.display === "none"
   ) {
     overlay.style.display = "block";
-    menu.classList.toggle("burger-open");
+    menu.classList.toggle("menu-open");
+    burgerImage.classList.toggle("burger_open");
+    burgerLine1.classList.toggle("burger_line_open_1");
+    burgerLine2.classList.toggle("burger_line_open_2");
     menu.style.right = "0%";
-    burgerImage.style.transform = "rotate(90deg)";
     body.style.overflow = "hidden";
   } else {
-    menu.classList.toggle("burger-open");
+    menu.classList.toggle("menu-open");
     overlay.style.display = "none";
+    menu.classList.toggle("menu-open");
+    burgerImage.classList.toggle("burger_open");
+    burgerLine1.classList.toggle("burger_line_open_1");
+    burgerLine2.classList.toggle("burger_line_open_2");
     menu.style.right = "-100%";
-    burgerImage.style.transform = "rotate(0deg)";
     body.style.overflow = "auto";
   }
 }
 
 burgerImage.addEventListener("click", openOrCloseBurger);
 overlay.addEventListener("click", openOrCloseBurger);
-// menuItem.forEach((element) =>
-//   element.addEventListener("click", openOrCloseBurger)
-// );
+menuItem.forEach((element) =>
+  element.addEventListener("click", openOrCloseBurger)
+);
